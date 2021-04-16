76c91025a272eb22c6234765d9af4eb8a8be295d
jackkkonggg <jqong@connect.ust.hk>
2021-04-16 17:04:27 +0800

    add placeholders for profile

--- hack-front/src/components/Profile.tsx
+++ hack-front/src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 
-import { Avatar } from '@material-ui/core';
+import { Avatar, Card, Chip, LinearProgress, Typography } from '@material-ui/core';
 
 import { selectCurrentNav } from 'models/selectors';
 import { useSelector } from 'react-redux';
@@ -9,6 +9,7 @@ import styled from 'styled-components';
 const ProfileContainer = styled.div`
   height: 100%;
   width; 100%;
+  padding: 16px;
   position: relative;
 
   display: flex;
@@ -17,16 +18,54 @@ const ProfileContainer = styled.div`
 `;
 
 const StyledAvatar = styled(Avatar)`
-  margin-top: 4rem;
+  margin-top: 3rem;
+  margin-bottom: 1rem;
   height: min(40vmin, 100px) !important;
   width: min(40vmin, 100px) !important;
 `;
+
+const ProgressContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLinearProgress = styled(LinearProgress)`
+  margin-right: 1rem;
+  width: 50%;
+`;
+
+const LevelIndicator = styled(Chip)`
+  min-width: 3rem;
+`;
+
+const StyledCard = styled(Card)`
+  border-radius: 8px !important;
+  height: 200px;
+  width: 100%;
+  margin: 0.5rem 0;
+`;
+
+const LevelProgress: FC = () => (
+  <ProgressContainer>
+    <StyledLinearProgress variant="determinate" value={80} />
+    <LevelIndicator label="LVL 6" variant="outlined" />
+  </ProgressContainer>
+);
+
+const EmptyCard: FC = () => null;
 
 const Profile: FC = () => {
   const currentNav = useSelector(selectCurrentNav);
   return (
     <ProfileContainer>
       <StyledAvatar />
+      <Typography variant="h6">User Name</Typography>
+      <Typography variant="body1">user@name.com</Typography>
+      <LevelProgress />
+      <StyledCard elevation={5} />
+      <StyledCard elevation={5} />
     </ProfileContainer>
   );
 };
