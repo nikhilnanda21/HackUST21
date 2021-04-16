b6028abcef9c6284a5cad8de091bc7f8fd7edc6c
jackkkonggg <jqong@connect.ust.hk>
2021-04-16 21:52:33 +0800

    update leaderboard data

--- hack-front/src/components/Leaderboard.tsx
+++ hack-front/src/components/Leaderboard.tsx
@@ -14,26 +14,61 @@ const StyledAvatar = styled(Avatar)`
 `;
 
 const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Name', width: 120 },
-  { field: 'aff', headerName: 'Affiliation', width: 120 },
+  { field: 'company', headerName: 'Company', width: 200 },
   {
     field: 'score',
     headerName: 'Score',
     type: 'number',
-    width: 120,
+    width: 150,
   },
 ];
 
 const rows = [
-  { id: 1, aff: 'Snow', name: 'Jon', score: 35 },
-  { id: 2, aff: 'Lannister', name: 'Cersei', score: 42 },
-  { id: 3, aff: 'Lannister', name: 'Jaime', score: 45 },
-  { id: 4, aff: 'Stark', name: 'Arya', score: 16 },
-  { id: 5, aff: 'Targaryen', name: 'Daenerys', score: null },
-  { id: 6, aff: null, name: 'Melisandre', score: 150 },
-  { id: 7, aff: 'Clifford', name: 'Ferrara', score: 44 },
-  { id: 8, aff: 'Frances', name: 'Rossini', score: 36 },
-  { id: 9, aff: 'Roxie', name: 'Harvey', score: 65 },
+  {
+    id: '0b20acab-9360-49b9-94ba-954f46742f7d',
+    company: 'Futurize',
+    score: 665,
+  },
+  {
+    id: '10452c15-bae7-43a2-8e89-c7b881ff69a7',
+    company: 'Lingoage',
+    score: 522,
+  },
+  {
+    id: '0a44d567-dd3d-456a-832a-04bfe9d00e0d',
+    company: 'Magnina',
+    score: 943,
+  },
+  {
+    id: '5ce37d8f-a68d-458d-a2b3-ec2a46f709a0',
+    company: 'Tropoli',
+    score: 984,
+  },
+  {
+    id: 'd3c7b6e1-bf51-44dd-9000-869f53d86f7e',
+    company: 'Cubicide',
+    score: 749,
+  },
+  {
+    id: 'ffd88af7-6c50-4384-b03b-74a22e97fdcf',
+    company: 'Pheast',
+    score: 598,
+  },
+  {
+    id: 'a7c83921-3d8a-4789-8ec4-9ebb69bdaae3',
+    company: 'Hometown',
+    score: 281,
+  },
+  {
+    id: '66ed8c62-ad8e-468f-9b61-2ac5050fffc9',
+    company: 'Buzzopia',
+    score: 253,
+  },
+  {
+    id: '11cf316d-f1e4-4e96-857b-12f0724345ac',
+    company: 'Enerforce',
+    score: 292,
+  },
 ];
 
 const LeaderboardContainer = styled.div`
