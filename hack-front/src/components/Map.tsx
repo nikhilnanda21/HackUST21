b820daa4d31392b405deefccf77bcfdd911054a3
jackkkonggg <jqong@connect.ust.hk>
2021-04-16 20:03:48 +0800

    add current location indicator

--- hack-front/src/components/Map.tsx
+++ hack-front/src/components/Map.tsx
@@ -1,6 +1,10 @@
-import React, { FC, useState } from 'react';
+import 'styles/Map.css';
 
-import GoogleMapReact from 'google-map-react';
+import React, { FC, useCallback, useEffect, useState } from 'react';
+
+import GoogleMapReact, { Coords } from 'google-map-react';
+import { setNotification } from 'models';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 
 const MapContainer = styled.div`
@@ -9,7 +13,16 @@ const MapContainer = styled.div`
   position: relative;
 `;
 
+const CurrentLocation: FC<Coords> = () => (
+  <div className="dot med">
+    <span className="point">
+      <span className="pulse" />
+    </span>
+  </div>
+);
+
 const Map: FC = () => {
+  const dispatch = useDispatch();
   const [mapState, setMapState] = useState({
     center: {
       lat: 22.3193,
@@ -17,14 +30,51 @@ const Map: FC = () => {
     },
     zoom: 11,
   });
+  const [center, setCenter] = useState(mapState.center);
+
+  // eslint-disable-next-line no-undef
+  const positionCallback: PositionCallback = useCallback((position: GeolocationPosition) => {
+    const {
+      coords: { latitude, longitude },
+    } = position;
+    setCenter({ lat: latitude, lng: longitude });
+  }, []);
+
+  useEffect(() => {
+    // Get Current Location
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        console.log('navigator.geolocation');
+        navigator.geolocation.getCurrentPosition(positionCallback, (positionError) => {
+          dispatch(
+            setNotification({
+              message: positionError.message,
+              severity: 'error',
+            }),
+          );
+        });
+      } else {
+        dispatch(
+          setNotification({
+            message: 'GeoLocation not supported, falling back to mock data',
+            severity: 'info',
+          }),
+        );
+      }
+    };
+    getLocation();
+  }, []);
 
   return (
     <MapContainer>
       <GoogleMapReact
         defaultCenter={mapState.center}
+        center={center}
         defaultZoom={mapState.zoom}
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY as string }}
-      />
+      >
+        <CurrentLocation {...center} />
+      </GoogleMapReact>
     </MapContainer>
   );
 };
