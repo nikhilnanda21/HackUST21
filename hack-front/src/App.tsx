2ca4ec251fdaf6514954ca3be8694c881d7a0e26
jackkkonggg <jqong@connect.ust.hk>
2021-04-16 17:08:39 +0800

    theme overriding

--- hack-front/src/App.tsx
+++ hack-front/src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
+import { ThemeProvider } from '@material-ui/styles';
+
 import BottomNavigation from 'components/BottomNavigation';
 import NavSwitch from 'components/NavSwitch';
+import theme from 'theme';
 
 import './App.css';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <NavSwitch />
       <BottomNavigation />
-    </>
+    </ThemeProvider>
   );
 }
 
